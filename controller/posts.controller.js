c3c097f8a0aab03d753fe49e3d7b2ed75c80e2c5
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-12-18 05:08:31 +0700

    Added update and delete

--- controller/posts.controller.js
+++ controller/posts.controller.js
@@ -14,6 +14,7 @@ const postsController = {
             })
         }
     },
+
     getById : async(req, res) => {
         try {
             const { id } = req.params
@@ -33,6 +34,38 @@ const postsController = {
             const { title, content } = req.body
             const sql = "insert into posts (title, content) values (?, ?)"
             const [rows, fields] = await pool.query(sql, [title, content])
+            res.json({
+                data: rows
+            })
+        } catch (error) {
+            console.log(error)
+            res.json({
+                state: "error"
+            })
+        }
+    },
+
+    update: async(req, res) => {
+        try {
+            const {title, content} = req.body
+            const { id } = req.params
+            const sql = "update posts set title = ?, content = ? where id = ?"
+            const [rows, fields] = await pool.query(sql, [title, content, id])
+            res.json({
+                data: rows
+            })
+        } catch (error) {
+            console.log(error)
+            res.json({
+                state: "error"
+            })
+        }
+    },
+
+    delete: async (req, res) => {
+        try {
+            const { id } = req.params
+            const [rows, fields] = await pool.query("delete from posts where id = ?" , [id])
             res.json({
                 data: rows
             })
