86d1b152cfb863966e5bbdf52e595e35e71a889a
Putra Ganda Dewata <putraganda.dewata@gmail.com>
2023-12-18 04:58:52 +0700

    Added example for getAll, getById, and create

---
+++ controller/posts.controller.js
@@ -0,0 +1,49 @@
+const pool = require("../database/index")
+
+const postsController = {
+    getAll: async(req, res) => {
+        try {
+            const [rows, fields] = await pool.query("select * from posts")
+            res.json({
+                data: rows
+            })
+        } catch (error) {
+            console.log(error)
+            res.json({
+                state: "error"
+            })
+        }
+    },
+    getById : async(req, res) => {
+        try {
+            const { id } = req.params
+            const [rows, fields] = await pool.query("select * from posts where id = ?", [id])
+            res.json({
+                data: rows
+            })
+        } catch (error) {
+            console.log(error)
+            res.json({
+                state: "error"
+            })
+        }
+    },
+    create: async(req, res) => {
+        try {
+            const { title, content } = req.body
+            const sql = "insert into posts (title, content) values (?, ?)"
+            const [rows, fields] = await pool.query(sql, [title, content])
+            res.json({
+                data: rows
+            })
+        } catch (error) {
+            console.log(error)
+            res.json({
+                state: "error"
+            })
+        }
+    }
+
+}
+
+module.exports = postsController
--- routes/posts.router.js
+++ routes/posts.router.js
@@ -4,5 +4,7 @@ const router = express.Router()
 const postsController = require("../controller/posts.controller")
 
 router.get("/", postsController.getAll)
+router.get("/:id", postsController.getById)
+router.post("/", postsController.create)
 
 module.exports = router
